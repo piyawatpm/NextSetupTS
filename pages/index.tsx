d802849638c63ee380effec5301e17289f5e7513
piyawatpm <90898951+piyawatpm@users.noreply.github.com>
2022-05-08 14:59:45 +1000

    commitfail

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const a = 5
+  const a = 1
   return (
     <div className={styles.container}>
       <Head>
