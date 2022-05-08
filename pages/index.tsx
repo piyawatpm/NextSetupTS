16dd4992d2186e041f30f5a9f31c537e0b518348
piyawatpm <90898951+piyawatpm@users.noreply.github.com>
2022-05-08 15:06:21 +1000

    piyawat

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
-  const a = 1
+  const a = 7
   return (
     <div className={styles.container}>
       <Head>
