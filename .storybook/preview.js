984d25c0f1c2d7886b826e6e4f251ea170787f1e
piyawatpm <90898951+piyawatpm@users.noreply.github.com>
2022-05-10 11:59:33 +1000

    feat: implement layout and sidebar

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -28,11 +28,24 @@ const customViewports = Object.fromEntries(
 // Allow Storybook to handle Next's <Image> component
 const OriginalNextImage = NextImage.default;
 
-Object.defineProperty(NextImage, 'default', {
-  configurable: false,
-  value: (props) => <OriginalNextImage {...props} unoptimized={true} />,
+// Object.defineProperty(NextImage, 'default', {
+//   configurable: false,
+//   value: (props) => <OriginalNextImage {...props} unoptimized={true} />,
+// });
+
+Object.defineProperty(NextImage, "default", {
+  configurable: true,
+  value: (props) => typeof props.src === 'string' ? (
+    <OriginalNextImage {...props} unoptimized blurDataURL={props.src} />
+  ) : (
+    <OriginalNextImage {...props} unoptimized />
+  ),
 });
 
+Object.defineProperty(NextImage, "__esModule", {
+  configurable: true,
+  value: true
+});
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
---
+++ components/layouts/primary/PrimaryLayout.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import styles from './PrimaryLayout.module.css';
+
+export interface IPrimaryLayout {
+    children?: React.ReactNode
+}
+
+const PrimaryLayout: React.FC<IPrimaryLayout> = ({ children }) => {
+    console.log("primary re-render");
+    
+  return (
+    <>
+      <Head>
+        <title>Primary Layout Example</title>
+      </Head>
+      <main className={styles.main}>{children}</main>
+    </>
+  );
+};
+
+export default PrimaryLayout;
